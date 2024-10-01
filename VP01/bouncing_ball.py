99e5808499bbad4740e2d0b0762efe43221a3063
AaronWu-train <aaronwu0218@gmail.com>
2024-10-01 10:49:56 +0800

    Add VP01 examples

---
+++ VP01/bouncing_ball.py
@@ -0,0 +1,20 @@
+from vpython import *
+
+g = 9.8
+size = 0.25  # ball radius
+
+scene = canvas(center=vec(0, 5, 0), width=600, background=vec(0.5, 0.5, 0))
+floor = box(length=30, height=0.01, width=4, color=color.blue)
+ball = sphere(radius=size, color=color.red, make_trail=1, trail_radius=size / 3)
+
+ball.pos = vec(-15.0, 10.0, 0.0)
+ball.v = vec(2.0, 0.0, 0.0)
+dt = 0.001
+
+while ball.pos.x < 15.0:
+    rate(1000)
+    ball.pos += ball.v * dt
+    ball.v.y += -g * dt
+
+    if ball.pos.y <= size and ball.v.y < 0:
+        ball.v.y = -ball.v.y
