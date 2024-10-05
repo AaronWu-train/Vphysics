75c0f720b2a5f843cdd859f0b2ce58b3dda51a81
AaronWu-train <aaronwu0218@gmail.com>
2024-10-05 21:41:14 +0800

    Update vp02.py

--- homeworks/vp02.py
+++ homeworks/vp02.py
@@ -1,10 +1,10 @@
 from vpython import *
 
 # Constants
-g = vector(0,-9.8,0)
+g = vector(0, -9.8, 0)
 k = 150000
 size, m = 0.2, 1
-L = 2 # length = 2.00 m
+L = 2  # length = 2.00 m
 
 N = 2
 
@@ -15,7 +15,7 @@ scene = canvas(
     width=600,
     background=vec(0.5, 0.5, 0),
 )
-ceiling = box(length=5, height=0.005, width=5, color=color.green, pos= vec(0, 0, 0))
+ceiling = box(length=5, height=0.005, width=5, color=color.green, pos=vec(0, 0, 0))
 
 springs = []
 for i in range(5):
@@ -24,26 +24,24 @@ for i in range(5):
 
 balls = []
 for i in range(5):
-    ball = sphere(
-        pos=vec(0.4 * i - 1, -L, 0), radius=size, color=color.red
-    )
+    ball = sphere(pos=vec(0.4 * i - 1, -L, 0), radius=size, color=color.red)
     ball.v = vec(0, 0, 0)
     ball.m = m
     balls.append(ball)
 
 
 for i in range(N):
-    balls[i].pos = vec(-sqrt(2**2 - 1.95**2) +0.4*i - 1, -1.95, 0)
+    balls[i].pos = vec(-sqrt(2**2 - 1.95**2) + 0.4 * i - 1, -1.95, 0)
 
 # plot 1
-instant_plot = graph(width = 400, align = 'left')
-k_plot = gcurve(graph = instant_plot, color = color.blue, width = 2.5)
-p_plot = gcurve(graph = instant_plot, color = color.orange, width = 2.5)
+instant_plot = graph(width=400, align="left")
+k_plot = gcurve(graph=instant_plot, color=color.blue, width=2.5)
+p_plot = gcurve(graph=instant_plot, color=color.orange, width=2.5)
 
 # plot 2
-average_plot = graph(width = 400, align = 'left')
-avg_k_plot = gcurve(graph = average_plot, color = color.blue, width = 2.5)
-avg_p_plot = gcurve(graph = average_plot, color = color.orange, width = 2.5)
+average_plot = graph(width=400, align="left")
+avg_k_plot = gcurve(graph=average_plot, color=color.blue, width=2.5)
+avg_p_plot = gcurve(graph=average_plot, color=color.orange, width=2.5)
 avg_k = 0
 avg_p = 0
 
@@ -58,21 +56,23 @@ while True:
     pe = 0
     for i in range(5):
         springs[i].axis = balls[i].pos - springs[i].pos
-        spring_force = - k * (mag(springs[i].axis) - L) * springs[i].axis.norm()
+        spring_force = -k * (mag(springs[i].axis) - L) * springs[i].axis.norm()
         balls[i].a = g + spring_force / m
         balls[i].v += balls[i].a * dt
         balls[i].pos += balls[i].v * dt
-        if (i < 4 and 0.4 >= mag(balls[i].pos - balls[i+1].pos)
-            and dot(balls[i].pos-balls[i+1].pos, balls[i].v-balls[i+1].v) <= 0) :
-            balls[i].v, balls[i+1].v = balls[i+1].v, balls[i].v
-        ke += 0.5 * balls[i].m * mag(balls[i].v)**2
+        if (
+            i < 4
+            and 0.4 >= mag(balls[i].pos - balls[i + 1].pos)
+            and dot(balls[i].pos - balls[i + 1].pos, balls[i].v - balls[i + 1].v) <= 0
+        ):
+            balls[i].v, balls[i + 1].v = balls[i + 1].v, balls[i].v
+        ke += 0.5 * balls[i].m * mag(balls[i].v) ** 2
         pe += m * g.y * (balls[i].pos.y + 2)
 
     k_plot.plot(pos=(t, ke))
-    p_plot.plot(pos=(t, pe))  
+    p_plot.plot(pos=(t, pe))
 
-    avg_k = (avg_k * (t - dt) + ke * dt) / t    
-    avg_p = (avg_p * (t - dt) + pe * dt) / t    
+    avg_k = (avg_k * (t - dt) + ke * dt) / t
+    avg_p = (avg_p * (t - dt) + pe * dt) / t
     avg_k_plot.plot(pos=(t, avg_k))
     avg_p_plot.plot(pos=(t, avg_p))
-    
